ba21cd85a46e084475718904d03b9584dca71927
coderider11 <94775366+coderider11@users.noreply.github.com>
2025-10-16 17:16:57 +0530

    Add files via upload

--- back-end/src/main/java/com/hackathon/lyzr/repository/TimesheetRepo.java
+++ back-end/src/main/java/com/hackathon/lyzr/repository/TimesheetRepo.java
@@ -1,1 +1,14 @@
+package com.hackathon.lyzr.repository;
+import java.util.List;
+import java.util.Optional;
+import com.hackathon.lyzr.model.TimesheetData;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface TimesheetRepo extends MongoRepository<TimesheetData,String> {
+
+  Optional<List<TimesheetData>> findByWeekStartingAndWeekEndingAndSubmitted(String start, String end, boolean isSubmitted);
+
+  Optional<TimesheetData> findByEmailId(String emailId);
+}
